b6b40303be2bf2eb5711294ad89a935b140017d3
Joshbigdaddy <joshua.cifuentes.romero@hotmail.com>
2017-03-25 18:27:32 +0100

    Changes to controllers

--- Acme-CnG/src/main/java/controllers/CustomerController.java
+++ Acme-CnG/src/main/java/controllers/CustomerController.java
@@ -1,48 +1,91 @@
-/* CustomerController.java
- *
- * Copyright (C) 2017 Universidad de Sevilla
- * 
- * The use of this project is hereby constrained to the conditions of the 
- * TDG Licence, a copy of which you may download from 
- * http://www.tdg-seville.info/License.html
- * 
- */
-
 package controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import services.ActorService;
+import services.CustomerService;
+import domain.Customer;
 
 @Controller
 @RequestMapping("/customer")
-public class CustomerController extends AbstractController {
+public class CustomerController {
 
-	// Constructors -----------------------------------------------------------
+	@Autowired
+	ActorService actorService;
 
-	public CustomerController() {
-		super();
+	@Autowired
+	CustomerService customerService;
+
+	@Autowired
+	UserDetailsService userDetailsService;
+
+	@RequestMapping(value = "/register")
+	public ModelAndView index() {
+		return this.createEditModelAndView(new Customer(), null);
 	}
 
-	// Action-1 ---------------------------------------------------------------		
+	@RequestMapping(value = "/register", method = RequestMethod.POST, params = "register")
+	public ModelAndView create(
+			@ModelAttribute("user") @Valid final Customer customer,
+			final BindingResult binding) {
 
-	@RequestMapping("/action-1")
-	public ModelAndView action1() {
 		ModelAndView result;
 
-		result = new ModelAndView("customer/action-1");
+		if (binding.hasErrors())
+			result = this.createEditModelAndView(customer, "wrong");
+		else
+			try {
+
+				final Md5PasswordEncoder md5PasswordEncoder = new Md5PasswordEncoder();
+
+				customer.getUserAccount().setPassword(md5PasswordEncoder
+
+				.encodePassword(customer.getUserAccount().getPassword(), null));
+
+				this.customerService.create(customer);
+
+				final UserDetails userDetails = this.userDetailsService
+						.loadUserByUsername(customer.getUserAccount()
+								.getUsername());
+
+				final UsernamePasswordAuthenticationToken auth =
+
+				new UsernamePasswordAuthenticationToken(userDetails, customer
+						.getUserAccount().getPassword(),
+
+				userDetails.getAuthorities());
+				if (auth.isAuthenticated())
+					SecurityContextHolder.getContext().setAuthentication(auth);
+				result = new ModelAndView("redirect:../");
+			} catch (final Throwable oops) {
+				result = this.createEditModelAndView(customer, "wrong");
+			}
 
 		return result;
+
 	}
-	
-	// Action-2 ---------------------------------------------------------------		
 
-	@RequestMapping("/action-2")
-	public ModelAndView action2() {
+	protected ModelAndView createEditModelAndView(final Customer customer,
+			final String message) {
+
 		ModelAndView result;
-
-		result = new ModelAndView("customer/action-2");
-
+		result = new ModelAndView("customer/register");
+		result.addObject("customer", customer);
+		result.addObject("message", message);
 		return result;
+
 	}
 }
